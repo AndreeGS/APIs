31a251dced75b5de3e84c31e81e70de1d8e877b8
AndreeGS <andre.guilherme2009@gmail.com>
2024-01-28 18:25:33 -0300

    Atualização

---
+++ API/API/Enums/StatusTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace API.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A fazer")]
+        Afazer ,
+        [Description("Em andamento")]
+        EmAndamento ,
+        [Description("Concluido")]
+        Concluido 
+    }
+}
---
+++ API/API/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class UsuarioModel
+    {
+        public int ID { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
