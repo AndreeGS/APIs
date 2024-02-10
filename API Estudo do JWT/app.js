a1189ddd340992739d24c73b2a1f19d848b6aa1f
André Guilherme <andre.guilherme2009@gmail.com>
2024-02-10 11:36:55 -0300

    Atualização

--- API Estudo do JWT/app.js
+++ API Estudo do JWT/app.js
@@ -19,6 +19,41 @@ app.get('/', (req, res) =>{
     res.status(200).json({msg: "Bem vindo a nossa API!"})
 })
 
+// Private Route
+app.get("/user/:id", checkToken, async (req,res) =>{
+    const id = req.params.id
+
+    //check if user exists
+    const user = await User.findById(id, '-password')
+
+    if(!user){
+        return res.status(404).json({msg: 'Usuário não encontrado'})
+    }
+
+    res.status(200).json({user})
+})
+
+
+function checkToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({msg:  "Acesso negado!"})
+    }
+
+    try{
+
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+
+        next()
+
+    }catch(error){
+        res.status(400).json({msg: "Token inválido"})
+    }
+}
+
 //Register User
 app.post('/auth/register', async(req, res) => {
     
@@ -100,7 +135,7 @@ app.post("/auth/login", async (req, res) =>{
 
         const token = jwt.sign(
             {
-                id: uder._id,
+                id: user._id,
             },
             secret,
         )
