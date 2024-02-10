1a92d467cca97fec633414cfe39dff232c4fe806
André Guilherme <andre.guilherme2009@gmail.com>
2024-02-09 21:32:25 -0300

    Atualização

--- Projeto/app.js
+++ Projeto/app.js
@@ -8,9 +8,125 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+//Config JSON response
+app.use(express.json())
+
+//Models
+const User = require('./Models/User')
+
 //Open Route - Public Route
 app.get('/', (req, res) =>{
     res.status(200).json({msg: "Bem vindo a nossa API!"})
 })
 
-app.listen(3000)
+//Register User
+app.post('/auth/register', async(req, res) => {
+    
+    const {name, email, password, confirmpassword} = req.body;
+
+    //validations
+    if(!name){
+        return res.status(422).json({msg: "O nome é obrigatório! "})
+    }
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatório! "})
+    }
+    if(!password){
+        return res.status(422).json({msg: "O senha é obrigatório! "})
+    }
+
+    if(password != confirmpassword){
+        return res.status(422).json({msg:"As senhas não conferem!"})
+    }
+
+    //Check if user exist
+    const userExist = await User.findOne({email: email})
+
+    if (userExist){
+        return res.status(422).json({msg:"Utilize outro email!"})
+    }
+
+    //Create passworld
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = new User({
+        name,
+        email,
+        password: passwordHash,
+    })
+
+    try{
+
+        await user.save()
+        res.status(201).json({msg: 'O usuário foi criado com sucesso'})
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({msg: 'Ocorreu um erro'})
+    }
+
+})
+
+// Login User
+app.post("/auth/login", async (req, res) =>{
+    const {email, password } = req.body;
+
+    //validations
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatório! "})
+    }
+    if(!password){
+        return res.status(422).json({msg: "A senha é obrigatório! "})
+    }
+
+    //Check if user exist
+    const user = await User.findOne({email: email})
+
+    if (!user){
+        return res.status(404).json({msg:"Usuário não encontrado!"})
+    }
+
+    // Check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword){
+        return res.status(422).json({msg: "Senha inválida!"})
+    }
+
+    try{
+        const secret = process.env.SECRET;
+
+        const token = jwt.sign(
+            {
+                id: uder._id,
+            },
+            secret,
+        )
+        res.status(200).json({msg: 'Autenticação realizada com sucesso!', token})
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({msg: 'Ocorreu um erro'})
+    }
+
+})
+
+
+
+
+
+//Credentials
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+mongoose.connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ek6x8ia.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(()=> {
+    app.listen(3000)
+    console.log('Conectou ao banco!')
+    })
+    .catch((err)=> console.log(err))
+
